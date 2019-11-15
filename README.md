ffd929901ca9ba9d5ab6dbd7c7ea606e9013240e
Oleg Kravchuk <oleg.kravchuk@gospime.com>
2019-11-16 00:13:15 +0200

    feat: express boomified

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express @hapi/boom
+Express HTTP-friendly error objects from @hapi
+
+[@hapi/boom](https://github.com/hapijs/boom)
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const Boom = require('@hapi/boom');
+
+module.exports = (request, response, next) => {
+  const boom = {};
+
+  const mapper = key => {
+    if (typeof Boom[key] !== 'function') return;
+    boom[key] = function() {
+      throw Boom[key].apply(Boom, arguments);
+    };
+  };
+
+  Object.getOwnPropertyNames(Boom).map(mapper);
+
+  response.boom = boom;
+
+  await next();
+};
